1c1b1a9dac971bfdc84772eb4eb9fe509f3898f5
Carlos A. Toruño P <carlos.toruno@gmail.com>
2023-04-27 11:46:58 -0600

    App second draft
    Now the app is running with the World Bank Official Boundaries

--- Code/boundaries_cleaning.py
+++ Code/boundaries_cleaning.py
@@ -1,42 +1,130 @@
-
 """
 Module Name:    GeoBoundaries Cleaaning
 Author:         Carlos Alberto Toruño Paniagua
 Date:           April 6th, 2023
-Description:    This module is focused in reading the Comprehensive Global Administrative Zones (CGAZ) dataset
-                provided by GeoBoundaries.org, filter the respective polygons and simplify their geometries for 
-                their use in the ROLI Map Generator App. The CGAZ dataset can be found here: 
-                https://www.geoboundaries.org/downloadCGAZ.html
+Description:    This module is focused in reading and preparing the World Bank Official Boundaries  
+                dataset provided by the World Bank Cartography Unit for their use in the ROLI-MAP-app.
+                The CGAZ dataset can be found here: 
+                https://datacatalog.worldbank.org/search/dataset/0038272/World-Bank-Official-Boundaries
+This version:   April 27th, 2023
 """
 
 import os
+import numpy as np
 import geopandas as gpd
 import pandas as pd
+from shapely.ops import unary_union
 
-# Loading GeoJSON file from GeoBoundaries.com
-path2gjson = os.path.join(os.path.dirname(__file__), 
-                          '..',
-                          "Data", 
-                          "geoBoundariesCGAZ_ADM0.geojson")
-raw_boundaries = gpd.read_file(path2gjson)
+# Defining path to data files
+path2data = os.path.join(os.path.dirname(__file__), 
+                         '..', 
+                         "Data/WB_geodata/")
+
+# Loading Boundaries GeoJSON
+raw_boundaries       = (gpd
+                        .read_file(path2data + "WB_countries_Admin0.geojson"))
+raw_boundaries       = raw_boundaries[["TYPE", "WB_A3", "CONTINENT", 
+                                       "REGION_UN", "SUBREGION", "REGION_WB", 
+                                       "NAME_EN", "WB_NAME", "WB_REGION", 
+                                       "geometry"]]
+
+# Adjusting Dependent Territories labeled as "Country"
+raw_boundaries.loc[raw_boundaries["WB_A3"] == "JEY", "TYPE"]    = "Dependency"
+raw_boundaries.loc[raw_boundaries["WB_A3"] == "GGY", "TYPE"]    = "Dependency"
+raw_boundaries.loc[raw_boundaries["WB_A3"] == "IMY", "TYPE"]    = "Dependency"
+raw_boundaries.loc[raw_boundaries["WB_A3"] == "SXM", "TYPE"]    = "Dependency"
+raw_boundaries.loc[raw_boundaries["WB_A3"] == "CUW", "TYPE"]    = "Dependency"
+raw_boundaries.loc[raw_boundaries["WB_A3"] == "ABW", "TYPE"]    = "Dependency"
+raw_boundaries.loc[raw_boundaries["WB_A3"] == "BES", "TYPE"]    = "Dependency"
+raw_boundaries.loc[raw_boundaries["WB_A3"] == "SXM", "TYPE"]    = "Dependency"
+raw_boundaries.loc[raw_boundaries["WB_A3"] == "TKL", "TYPE"]    = "Dependency"
+raw_boundaries.loc[raw_boundaries["TYPE"]  == "Lease", "WB_A3"] = "XXX"
+
+# Adjusting Geographical allocaations to match WJP's allocation
+raw_boundaries.loc[raw_boundaries["WB_A3"] == "MEX", "SUBREGION"] = "Central America"
+raw_boundaries.loc[raw_boundaries["WB_A3"] == "MLT", "REGION_WB"] = "Europe & Central Asia"
+
+# Loading Disputed Territories GeoJSON
+disputed_territories = (gpd
+                        .read_file(path2data + "WB_Admin0_disputed_areas.geojson")
+                        .drop(6)
+                        .assign(TYPE="Disputed"))
+disputed_territories = disputed_territories[["TYPE", "WB_A3", "CONTINENT", 
+                                             "REGION_UN", "SUBREGION", "REGION_WB", 
+                                             "NAME_EN", "WB_NAME", "WB_REGION", 
+                                             "geometry"]]
+disputed_territories["WB_grouped_A3"] = disputed_territories["WB_A3"]
+
+# The following steps were needed when using the GeoBoundaries data. However,
+# Now that we are using the World Bank Data, these steps are not required anymore.
 
 # Simplify polygons with a tolerance of 0.01
-simplified_boundaries = raw_boundaries.simplify(tolerance = 0.01, 
-                                                preserve_topology = True)
+# simplified_boundaries = boundaries_adt.simplify(tolerance = 0.01, 
+#                                                 preserve_topology = True)
 
 # Replacing old and accurate geometries for simplified ones
-gboundaries = gpd.GeoDataFrame(raw_boundaries.drop(columns = ['geometry']), 
-                               geometry = simplified_boundaries, 
-                               crs = raw_boundaries.crs)
+# gboundaries = gpd.GeoDataFrame(raw_boundaries.drop(columns = ['geometry']), 
+#                                geometry = simplified_boundaries, 
+#                                crs = raw_boundaries.crs)
 
 # Filtering Antarctica and those polygons without a shapeGroup value
-filtered_boundaries = gboundaries.query("shapeGroup != 'ATA' and not(shapeGroup.isna())")
+# filtered_boundaries = gboundaries.query("shapeGroup != 'ATA' and not(shapeGroup.isna())")
+
+# Defining a function to group dependencies to their "Parent State"
+def group_dependencies(row):
+    code = row["WB_A3"]
+    if code in ["ASM", "UMI", "GUM", "MNP", "PRI", "VIR"]:
+        return "USA"
+    elif code in ["AIA", "BMU", "IOT", "VGB", "CYM", "FLK", "GIB", "GGY", 
+                  "IMY", "JEY", "MSR", "PCN", "SHN", "SGS", "TCA"]:
+        return "GBR"
+    elif code in ["ABW", "BES", "CUW", "SXM"]:
+        return "NLD"
+    elif code in ["PYF", "ATF", "NCL", "BLM", "MAF", "SPM", "WLF"]:
+        return "FRA"
+    elif code in ["CXR", "CCK", "HMD", "NFK"]:
+        return "AUS"
+    elif code in ["COK", "NIU", "TKL"]:
+        return "NZL"
+    elif code in ["FRO"]: # Greenland "GRL" stays as a separate territory
+        return "DNK"
+    elif code in ["HKG", "MAC"]:
+        return "CHN"
+    else:
+        return row["WB_A3"]
+
+# Creating a grouped WB code
+raw_boundaries["WB_grouped_A3"] = raw_boundaries.apply(group_dependencies, 
+                                                       axis = 1)
+
+# Transforming variables from all DEPENDENCY territories to NaN
+raw_boundaries.loc[raw_boundaries["TYPE"] == "Dependency", 
+                   ~(raw_boundaries
+                     .columns
+                     .isin(["WB_grouped_A3", "geometry"]))] = np.nan
+
+# Dissolving the geo data frame
+raw_boundaries = (raw_boundaries
+                  .dissolve(by      = "WB_grouped_A3",
+                            aggfunc = "first")).reset_index()
+
+# Unifying TYPE == COUNTRY
+raw_boundaries.loc[raw_boundaries["TYPE"] == "Sovereign country", "TYPE"] = "Country"
+
+# Concatenating boundaries with disputed territories
+boundaries = pd.concat([raw_boundaries, disputed_territories])
+boundaries = boundaries.drop(["WB_grouped_A3", "CONTINENT", "WB_REGION", "NAME_EN"], 
+                             axis = 1)
 
 # Save the updated GeoJSON to a file
 path4saving = os.path.join(os.path.dirname(__file__), 
                            '..',
-                           "Data", 
-                           "data4app.geojson")
-filtered_boundaries.to_file(path4saving, 
-                            driver="GeoJSON")
+                           "Data")
+boundaries.to_file(path4saving + "/data4app.geojson", 
+                   driver="GeoJSON")
+
+# Converting data to a Pandas DataFrame and saving it as CSV
+(pd
+ .DataFrame(boundaries.drop(columns="geometry"))
+ .to_csv(path4saving + "/data4app.csv"))
 
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import streamlit as st
+import streamlit_toggle
 
 st.set_page_config(
     page_title = "Map Generator",
@@ -18,10 +19,11 @@ with open("styles.css") as stl:
 @st.cache_data
 def load_data():
     # Loading data
-    boundaries = gpd.read_file("Data/data4app.geojson")
-    roli_data  = pd.read_excel("Data/ROLI_data.xlsx")
-    data       = {"boundaries": boundaries,
-                  "roli": roli_data}
+    boundaries        = gpd.read_file("Data/data4app.geojson")
+    roli_data         = pd.read_excel("Data/ROLI_data.xlsx")
+    roli_data["year"] = roli_data["year"].apply(str)
+    data              = {"boundaries": boundaries,
+                         "roli": roli_data}
     return data 
 
 master_data = load_data()
@@ -81,6 +83,7 @@ variable_labels = ["Rule of Law Index Overall Score",
 
 # Intro text
 st.title("ROLI Map Generator")
+
 st.markdown(
     """
     <p class='jtext'>
@@ -96,14 +99,13 @@ st.markdown(
 
     <ol>
         <li class='jtext'>
-            Select the extension of the map. It can be either a global map or limited to a specific region.
+            Define the geographical extension of your map. It can be either a global map or limited to a specific region.
         </li>
         <li class='jtext'>
-            Select the score and year that you would like the map to display from the dropdown menu.
+            Select the data that you would like the map to display from the available options.
         </li>
         <li class='jtext'>
-            You can further customize your map by adding color breaks or changing the color key. Once that you are done
-            with the customization, click on <b style="color:#003249">"Update my custom setting please"</b>.
+            You can further customize your map by adding color breaks or changing the color key. You don't need to set a color for every country, you just need to define the major color breaks that your Choropleth Map should have. The app will then, do the rest. 
         </li>
         <li class='jtext'>
             Once you are ready, click on the <b style="color:#003249">"Let's rock!!"</b> button to visualize your map.
@@ -129,81 +131,144 @@ st.markdown(
 
 st.markdown("""---""")
 
-# Creating a container for the general options
-goptions_container = st.container()
-with goptions_container:
+# Creating a container for the geographical extension of the map
+extension_container = st.container()
+with extension_container:
 
-    # Inputs Box Title
-    st.markdown("<h4>General Options:</h4>",
+    # Extension Container Title
+    st.markdown("<h4>Step 1: Define the geographical extension of your map</h4>",
                     unsafe_allow_html = True)
         
     # Extension input
+    extension_help = '''
+    The extension refers to the extension of your desired map. A world map is straightforward, but if you want to draw a regional map we need you to fulfill additional options about the geographical extension of your map.
+    '''
+    regions_help = '''
+    The World Bank classification divides countries and territories in seven regions,
+    an it is driven by geography and development criteria. This is the classification used by WJP to present their results. The United Nations classification divides
+    countries and territories in 5 continents and 22 subregions.
+    '''
+
+    st.markdown(
+        """
+        <b>!!!REGIONAL EXTENSIONS ARE NOT AVAILABLE YET!!!<br>
+        However, you can visualize the available regions.</b>
+        """,
+        unsafe_allow_html = True)
+
     extension = st.radio("Select an extension for your map:", 
                         ["World", "Regional"],
-                        horizontal = True)
+                        horizontal = True,
+                        help       = extension_help)
 
-    # Possible Regions
-    regions = ["The Americas", "Eurasia and Africa",  "East Asia and Pacific", "Eastern Europe and Central Asia", 
-            "EU, EFTA, and North America", "Latin America and Caribbean", "Middle East and North Africa",
-            "South Asia", "Sub-Saharan Africa"]
-
-    extension_container = st.empty()
+    # Available Regions
+    UN_regions    = master_data["boundaries"]["REGION_UN"].unique().tolist()
+    UN_subregions = master_data["boundaries"]["SUBREGION"].unique().tolist()
+    WB_regions    = master_data["boundaries"]["REGION_WB"].unique().tolist()
 
     # Conditional display
     if extension == "Regional":
         
-        # Dropdown menu for regions
-        region = extension_container.selectbox("Select a region:", 
-                                               regions)
-    else:
-        region = None
+        # Which regions are we working with?
+        regions_div = st.radio("Which region classification would you like to use?",
+                              ["World Bank", "United Nations"],
+                              horizontal = True,
+                              help       = regions_help)
+        
+        if regions_div == "World Bank":
 
-# Search Box Inputs
-with st.form(key = "inputsForm"):
-    st.markdown("<h4>What are we mapping?</h4>",
-                unsafe_allow_html = True)
+            # Defining the filtering variable
+            regfilter = "REGION_WB"
+
+            # Dropdown menu for regions
+            selected_regions = st.multiselect("Select the regions you would like to work with:", 
+                                              WB_regions,
+                                              help = "You can select more than one")
+            
+        if regions_div == "United Nations":
+
+            # Defining the filtering variable
+            regfilter = "SUBREGION"
+
+            # Dropdown menu for regions
+            regions = st.multiselect("Select the regions you would like to work with:",  
+                                     UN_regions,
+                                     help = "You can select more than one")
+            
+            # Dropdown menu for subregions
+            listed_subregions = master_data["boundaries"][master_data["boundaries"]["REGION_UN"].isin(regions)]["SUBREGION"].unique().tolist()
+            selected_regions = st.multiselect("Select the regions you would like to work with:", 
+                                              listed_subregions,
+                                              help = "You can select more than one")
+
+    else:
+        selected_regions = None
+        regfilter        = None
+
+st.markdown("""---""")
+
+# Creating a container for the data selection
+data_container = st.container()
+with data_container:
+
+    # Data Container Title
+    st.markdown("<h4>Step 2: Select the scores to be displayed in your map</h4>",
+                    unsafe_allow_html = True)
     
     # Searchbox to define a target variable
     available_variables = dict(zip(master_data["roli"].iloc[:, 4:].columns.tolist(),
                                    variable_labels))
-    available_years = master_data["roli"]["year"].unique().tolist()
+    available_years = sorted(master_data["roli"]["year"].unique().tolist(),
+                             reverse = True)
     target_variable = st.selectbox("Select a variable from the following list:",
                                    list(available_variables.keys()),
                                    format_func=lambda x: available_variables[x])
     target_year     = st.selectbox("Select which year do you want to display from the following list:",
                                    available_years)
-    
-    # Container for cutomizing options
-    with st.expander("Do you want to customize your map?"):
 
-        # Defining default colors
-        default_colors = [["#B49A67"],
-                          ["#B49A67", "#001A23"],
-                          ["#98473E", "#B49A67", "#001A23"],
-                          ["#98473E", "#B49A67", "#395E66", "#001A23"],
-                          ["#98473E", "#B49A67", "#7A9E7E", "#395E66", "#001A23"]]
-        
-        color_breaks = []
-        
-        # Dropdown menu for number of color breaks
-        ncolors = st.number_input("Select number of color breaks", 2, 5, 4)
-        st.markdown("<b>Select or write down the color codes for your legend</b>:",
+st.markdown("""---""")
+
+# Creating a container for the customization options
+customization = st.container()
+with customization:
+
+    # Data Cistomization Container Title
+    st.markdown("<h4>Step 3: Customize your map</h4>",
                     unsafe_allow_html = True)
-        cols    = st.columns(ncolors)
 
-        for i, x in enumerate(cols):
-            input_value = x.color_picker(f"Break #{i+1}:", 
-                                         default_colors[ncolors-1][i],
-                                         key = f"break{i}")
-            x.write(str(input_value))
-            color_breaks.append(input_value)
-        
-        # Update customization button
-        custom_button = st.form_submit_button(label = "Update my custom settings please")
+    # Defining default colors
+    default_colors = [["#B49A67"],
+                        ["#B49A67", "#001A23"],
+                        ["#98473E", "#B49A67", "#001A23"],
+                        ["#98473E", "#B49A67", "#395E66", "#001A23"],
+                        ["#98473E", "#B49A67", "#7A9E7E", "#395E66", "#001A23"]]
+    
+    color_breaks = []
 
-    # Generate Button
-    st.write("Don't forget to update your custom settings if you want to rock...")
-    submit_button = st.form_submit_button(label = "Let's rock!!")
+    # Dropdown menu for number of color breaks
+    ncolors = st.number_input("Select number of color breaks", 2, 5, 4)
+    st.markdown("<b>Select or write down the color codes for your legend</b>:",
+                unsafe_allow_html = True)
+    cols    = st.columns(ncolors)
+
+    for i, x in enumerate(cols):
+        input_value = x.color_picker(f"Break #{i+1}:", 
+                                        default_colors[ncolors-1][i],
+                                        key = f"break{i}")
+        x.write(str(input_value))
+        color_breaks.append(input_value)
+
+st.markdown("""---""")
+
+# Creating a container for the saving options
+saving = st.container()
+    
+with saving:
+    #Saving Options Title
+    st.markdown("<h4>Step 4: Draw your map</h4>",
+                unsafe_allow_html = True)
+
+    submit_button = st.button(label = "Let's rock!!")
 
 # Server
 if submit_button:
@@ -213,7 +278,7 @@ if submit_button:
 
     # Merge the two DataFrames on a common column
     data4map = master_data["boundaries"].merge(filtered_roli,
-                                               left_on  = "shapeGroup", 
+                                               left_on  = "WB_A3", 
                                                right_on = "code",
                                                how      = "left")
     
